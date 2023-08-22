4a72a72606bcd87c72d1e432e21583f8dbe57dc3
seulgi9256 <sgij56@gmail.com>
2023-08-22 12:21:42 +0900

    2023.08.22

--- JAVA MISSION/src/JavaMission_04/_4.java
+++ JAVA MISSION/src/JavaMission_04/_4.java
@@ -6,20 +6,21 @@ public class _4 {
 	public static void main(String[] args) {
 		// 정수 하나를 입력 받아, 정수형 변수 N에 초기화시키고
 		// *을 1 3 5 7 9로 출력
-		int num = 1;
+		int j = 1;
 		
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		for(int i = 0; i < N; i++) { // 빈칸은 N-1씩
-			
-			for(int j=0; j<num; j++) {
+		for(int i=1 ; i <= N; i++) { // 빈칸은 N-1씩
+			for(j=N; j > i; j--) {
+				System.out.print(" ");
+			}
+			for(j=1; j < 2*i; j++) {
 				System.out.print("*");
 			}
-			num +=2;
+
 			System.out.println();
 		}
 		sc.close();	 
-
 	}
 }
--- JAVA MISSION/src/JavaMission_24to27/Driver.java
+++ JAVA MISSION/src/JavaMission_24to27/Driver.java
@@ -13,7 +13,31 @@ public class Driver {
 	static int speed = 0;
 	static Scanner sc = new Scanner(System.in);
 	
-	
 	// 구동 프로그램 시작
+	// 드론 : 모터 온, 속도 빠르게, 속도 느리게, 모터 오프, 배터리 교환
+	// RC카 : 시동걸기, 속도 빠르게, 속도 느리게, 시동끄기, 배터리 교환
+	
+	public static void main(String[] args) {
+		RemoteControl drone = new Drone();
+		drone.turnOn();
+		drone.setSpeed(200);
+		drone.setSpeed(30);
+		drone.turnOff();
+		RemoteControl.changeBattery();
+		System.out.println();
+		
+		RemoteControl rcCar = new RcCar();
+		rcCar.turnOn();
+		rcCar.setSpeed(100);
+		rcCar.setSpeed(50);
+		rcCar.turnOff();
+		RemoteControl.changeBattery();
+		
+	}
+	
+	
+	
+	
+	
 	
 }
